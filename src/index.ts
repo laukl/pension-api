3ef6756d200c477cf57d0b6dfc38c5fe88f439e7
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-05-11 00:48:26 +0100

    feat: add boilerplate fastify

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
