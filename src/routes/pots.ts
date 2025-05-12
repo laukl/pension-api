5384ca05e47cc4dfa455b3ff04915ee4c222536f
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-05-12 17:26:09 +0100

    feat: add get pot by id

--- src/routes/pots.ts
+++ src/routes/pots.ts
@@ -6,11 +6,21 @@ const getPotsQueryParamsSchema = z.object({
   name: z.string().optional(),
 });
 
+const getPotByIdSchema = z.object({ id: z.string() });
+
 export default function (fastify: FastifyInstance) {
   fastify.get("/pots", (req) => {
     const params = getPotsQueryParamsSchema.parse(req.query);
     return prisma.pensionPot.findMany({
       where: { potName: { contains: params.name } },
+      include: { provider: true },
+    });
+  });
+
+  fastify.get("/pots/:id", (req) => {
+    const params = getPotByIdSchema.parse(req.params);
+    return prisma.pensionPot.findUnique({
+      where: { id: params.id },
       include: { provider: true },
     });
   });
