14403f5cce208f58febffba571046221f3c182fa
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-05-12 21:19:37 +0100

    feat: add searches status filter

--- src/routes/searches.ts
+++ src/routes/searches.ts
@@ -1,9 +1,21 @@
+import { SearchStatus } from "@prisma/client";
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { ValidationError } from "../lib/error.js";
 import prisma from "../lib/prisma.js";
 
+const getSearchesQueryParamsSchema = z.object({
+  status: z.nativeEnum(SearchStatus).optional(),
+});
+
 export default function (fastify: FastifyInstance) {
-  fastify.get("/searches", async () => {
+  fastify.get("/searches", (req) => {
+    const result = getSearchesQueryParamsSchema.safeParse(req.query);
+    if (!result.success) {
+      throw new ValidationError(result.error.issues);
+    }
     return prisma.potSearch.findMany({
+      where: { status: result.data.status },
       include: { pot: { include: { provider: true } } },
     });
   });
