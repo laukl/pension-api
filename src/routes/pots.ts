7cd17d380d2ebb81a984374e276b3916187c29a1
M Lau <63963445+tofuuudon@users.noreply.github.com>
2025-05-12 17:07:02 +0100

    feat: add searches and relation

--- src/routes/pots.ts
+++ src/routes/pots.ts
@@ -2,7 +2,14 @@ import { FastifyInstance } from "fastify";
 import prisma from "../lib/prisma.js";
 
 export default function (fastify: FastifyInstance) {
-  fastify.get("/pots", async () => {
-    return await prisma.pensionPot.findMany();
+  fastify.get("/pots", () => {
+    return prisma.pensionPot.findMany({ include: { provider: true } });
+  });
+
+  fastify.get("/pots/pensions", () => {
+    return prisma.pensionPot.findMany({
+      where: { searches: { none: {} } },
+      include: { provider: true },
+    });
   });
 }
--- src/routes/searches.ts
+++ src/routes/searches.ts
@@ -3,6 +3,8 @@ import prisma from "../lib/prisma.js";
 
 export default function (fastify: FastifyInstance) {
   fastify.get("/searches", async () => {
-    return await prisma.potSearch.findMany({ include: { pot: true } });
+    return prisma.potSearch.findMany({
+      include: { pot: { include: { provider: true } } },
+    });
   });
 }
